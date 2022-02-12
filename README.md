664e5a09763c7fe0deb1a139cba105f05c24b712
nioke-dev <nioke8090@gmail.com>
2022-02-12 09:40:41 +0700

    second_commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-JS-With-Laravel-8-API
-React JS With Laravel 8 API di channel funda of web IT
--- latihan/routes/api.php
+++ latihan/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('students', [StudentController::class, 'index']);
+Route::post('/add-student', [StudentController::class, 'store']);
+Route::get('/edit-student/{id}', [StudentController::class, 'edit']);
+Route::put('/update-student/{id}', [StudentController::class, 'update']);
+Route::delete('/delete-student/{id}', [StudentController::class, 'destroy']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
